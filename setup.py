7de9e23cbbe616d4e50111faee58377723d4cbe2
Jari Turkia <jmjt@jari.turkia.name>
2021-08-05 14:20:59 +0300

    Added setup and requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='vacdec',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Hanno Böck',
+    author_email='',
+    description='',
+    python_requires='>=3.7, <4',
+    install_requires=['base45', 'cbor2', 'pillow', 'pyzbar', 'cose'],
+    scripts=['cert-check.py'],
+)
